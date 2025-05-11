12aaaf1142c4dcc996fae4ffa07385bac8145c60
Nicholas Amancio <nick.amancio44@gmail.com>
2025-05-11 13:50:18 -0700

    added endpoints

--- server/server.py
+++ server/server.py
@@ -12,24 +12,44 @@ CORS(app)
 
 def connect_db():
     myclient = pymongo.MongoClient(MONGODB_URI)
-    mydb = myclient["guests"]
+    mydb = myclient['guests']
     print("connected to db")
     return mydb
 
 @app.route("/addUser", methods=["POST"])
 def add_user():
-    conn = connect_db()
+    db = connect_db()
     try:
-        names = conn["names"]
-        print(names)
-        print(request.text)
-        """ name = {"name": request.text("name")}
-        names.insert_one(name)
-        return jsonify({"name": name}), 200 """
+        names = db['names']
+        data = request.get_json()
+        if not data or 'name' not in data:
+            return jsonify({"error": "Missing name"}), 400
+        name = {"name": data['name']}
+        result = names.insert_one(name)
+        return jsonify({
+            "_id": str(result.inserted_id),
+            "name": data['name']
+        }), 200
     except Exception as e:
         print("failed to add name")
         return jsonify({f"error": "failed to add name"}), 500
-
+    
+@app.route("/getUsers", methods=["GET"])
+def get_users():
+    db = connect_db()
+    try:
+        names = db['names']
+        result = []
+        
+        for name in names.find():
+            name['_id'] = str(name['_id'])
+            result.append(name)
+        return jsonify({
+            "names": result
+        }), 200
+    except Exception as e:
+        print("failed to retrieve names")
+        return jsonify({f"error": "failed to retrieve names"}), 500
 
 
 if __name__ == "__main__":
